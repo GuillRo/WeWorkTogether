306f8b314e16ff48879e01f72afc0625739277fa
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-13 09:04:14 -0700

    Back end seed with photos for workspaces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_140736) do
+ActiveRecord::Schema.define(version: 2019_03_13_160022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_140736) do
     t.bigint "workspace_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["workspace_id"], name: "index_photos_on_workspace_id"
   end
 
