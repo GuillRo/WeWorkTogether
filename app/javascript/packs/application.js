707f263c2e319614caa411195b0e2c0bfa04e120
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-13 04:01:25 -0700

    Back-end geocode

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,7 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initUpdateNavbarHomeOnScroll } from '../components/navbarhome';
-// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-// import { initMapbox } from '../plugins/init_mapbox';
-
 
 initUpdateNavbarOnScroll();
 initUpdateNavbarHomeOnScroll();
-// initMapbox();
+
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,4 +1,6 @@
 class Workspace < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_many :photos
   has_many :service_lists
