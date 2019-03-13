34d4a774924131a6977f6f64df1c5326a10adcd5
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-13 03:46:06 -0700

    Back-end geomap

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,8 +1,20 @@
 class WorkspacesController < ApplicationController
   # skip_before_action :authenticate_user!, only: :index
-  
+
   def index
-    @workspaces = Workspace.all
+    # @workspaces = Workspace.all
+    if params[:query].present?
+      @workspaces = Workspace.near(params[:query], 50)
+    else
+      @workspaces = Workspace.where.not(latitude: nil, longitude: nil)
+    end
+    @markers = @workspaces.map do |workspace|
+      {
+        lng: workspace.longitude,
+        lat: workspace.latitude,
+        infoWindow: render_to_string(partial: "shared/infowindow", locals: { workspace: workspace })
+      }
+    end
   end
 
   def show
