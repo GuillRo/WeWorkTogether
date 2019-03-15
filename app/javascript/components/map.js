630565818feb3b6f23c9fc48f9a4b5da58235cbb
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-15 17:23:03 +0100

    added

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,6 +1,7 @@
 const mapAction = () => {
   const mapButton = document.getElementById("map-button");
-  const mapBox = document.querySelector('#map');
+  if (mapButton) { 
+    const mapBox = document.querySelector('#map');
   mapButton.addEventListener("click", () => {
     mapBox.style.cssText = 'top: 0vh !important';
     mapX.style.display = "block";
@@ -12,6 +13,8 @@ const mapAction = () => {
     mapX.style.visibility = "hidden";
     mapBox.style.cssText = 'top: 150vh !important';
   })
+}
+ 
 }
 
 export { mapAction };
